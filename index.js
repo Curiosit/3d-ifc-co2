3a39e22e8b121ef7b465b0afa521094d570b8dc5
Curiosit <mailing.bot@gmail.com>
2023-12-18 12:24:45 +0100

    up

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function showModal() {
   }
   
   // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
-  const newProjectBtn = document.getElementById("new-projects-btn")
+  const newProjectBtn = document.getElementById("new-project-btn")
   newProjectBtn.addEventListener("click", showModal)
