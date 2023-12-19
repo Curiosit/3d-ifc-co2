c7eab3541c66f5cd7366b678f11d211a2d16c35e
Curiosit <mailing.bot@gmail.com>
2023-12-19 20:16:34 +0100

    up

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-function showModal() {
-    const modal = document.getElementById("new-project-modal")
+function showModal(id) {
+  const modal = document.getElementById(id)
+  if (modal) {
     modal.showModal()
+  } else {
+    console.warn("The provided modal wasn't found. ID: ", id)
   }
-  
-  // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
-  const newProjectBtn = document.getElementById("new-project-btn")
-  newProjectBtn.addEventListener("click", showModal)
+}
+
+// This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
+const newProjectBtn = document.getElementById("new-project-btn")
+if (newProjectBtn) {
+  newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+} else {
+  console.warn("New projects button was not found")
+}
+
+const projectForm = document.getElementById("new-project-form")
+if (projectForm) {
+  projectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    const project = {
+      name: formData.get("name"),
+      description: formData.get("description"),
+      status: formData.get("status"),
+      userRole: formData.get("userRole"),
+      finishDate: formData.get("finishDate"),
+    }
+    console.log(project)
+  })
+} else {
+	console.warn("The project form was not found. Check the ID!")
+}
