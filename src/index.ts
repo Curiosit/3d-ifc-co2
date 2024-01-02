f5427171546471a05131dde659288da95bd60934
Curiosit <mailing.bot@gmail.com>
2024-01-02 23:45:49 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import { formatDate, modifyDateInput } from "./utils/utils"
 import { showModal } from "./utils/utils"
 import { v4 as uuidv4 } from 'uuid'
 
+import { OBJLoader} from "three/examples/jsm/loaders/OBJLoader"
+import { MTLLoader} from "three/examples/jsm/loaders/MTLLoader"
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import * as THREE from "three"
 
 
@@ -135,27 +139,77 @@ if (importProjectsBtn) {
 const scene = new THREE.Scene()
 
 const viewerContainer = document.getElementById("viewer-container") as HTMLElement
-const containerDimensions = viewerContainer.getBoundingClientRect()
-const aspectRatio = containerDimensions.width / containerDimensions.height
-const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+
+
+const camera = new THREE.PerspectiveCamera(75)
 camera.position.z = 5
 
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true})
 viewerContainer.append(renderer.domElement)
 
-renderer.setSize(containerDimensions.width, containerDimensions.height)
-
+function resizeViewer() {
+  const containerDimensions = viewerContainer.getBoundingClientRect()
+  renderer.setSize(containerDimensions.width, containerDimensions.height)
+  const aspectRatio = containerDimensions.width / containerDimensions.height
+  camera.aspect = aspectRatio
+  camera.updateProjectionMatrix()
+}
+window.addEventListener("resize", resizeViewer)
+resizeViewer()
 
 const boxGeometry= new THREE.BoxGeometry()
 const material = new THREE.MeshStandardMaterial()
 const cube = new THREE.Mesh(boxGeometry, material)
-scene.add(cube)
+//scene.add(cube)
 
 const directionalLight = new THREE.DirectionalLight()
 const ambientLight = new THREE.AmbientLight()
-
+ambientLight.intensity = 0.4
 scene.add(directionalLight)
 scene.add(ambientLight)
 
-renderer.render(scene, camera)
+const cameraControls = new OrbitControls(camera, viewerContainer)
+
+function renderScene() {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(renderScene)
+}
+renderScene()
+
+const axes = new THREE.AxesHelper()
+scene.add(axes)
+const grid = new THREE.GridHelper()
+grid.material.transparent = true
+grid.material.opacity = 0.4
+grid.material.color = new THREE.Color("#808080")
+scene.add(grid)
+
+const gui = new GUI()
+const cubeControls = gui.addFolder("Cube")
+
+
+cubeControls.add(cube.position, "x", -10, 10, 1)
+cubeControls.add(cube.position, "y", -10, 10, 1)
+cubeControls.add(cube.position, "z", -10, 10, 1)
+cubeControls.add(cube, "visible")
+cubeControls.addColor(cube.material, "color")
+
+const lightControls = gui.addFolder("Light")
+lightControls.add(directionalLight, "intensity", 0, 1, 0.1)
+
+lightControls.add(directionalLight.position, "x", -10, 10, 1)
+lightControls.add(directionalLight.position, "y", -10, 10, 1)
+lightControls.add(directionalLight.position, "z", -10, 10, 1)
+
+const objLoader = new OBJLoader()
+const mtlLoader = new MTLLoader()
+
+objLoader.load("../assets/Gear/Gear1.obj", (mesh) => {
+  scene.add(mesh)
+})
+
+mtlLoader.load("../assets/Gear/Gear1.mtl", (materials) => {
+  materials.preload()
+  objLoader.setMaterials(materials)
+})
