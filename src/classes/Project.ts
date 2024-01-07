2d2a21c795bda6660a78e4a9a3d3b2064979a527
Curiosit <mailing.bot@gmail.com>
2024-01-07 21:01:38 +0100

    Update Project.ts

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -32,7 +32,7 @@ export class Project implements IProject {
   id: string;
 
   //Class internals
-  ui: HTMLDivElement;
+  
   initials: string;
   inColor: HexColor;
   taskUI: HTMLDivElement;
@@ -51,7 +51,7 @@ export class Project implements IProject {
     this.loadTasks();
     this.setTaskUI () 
     
-    this.setUI();
+    
     
   }
 
@@ -81,44 +81,10 @@ export class Project implements IProject {
       this[key] = data[key];
     }
     this.initials = uppercaseInitials(this.name);
-    this.setUI;
+    
   }
 
-  setUI() {
-    //if (this.ui) {return}
-    this.ui = document.createElement("div");
-    this.ui.className = "project-card";
-    this.ui.id = this.id;
-    this.ui.innerHTML = `
-        <div class="card-header">
-            <p class="initials" style="background: ${this.inColor}">${
-      this.initials
-    }</p>
-            <div>
-              <h4>${this.name}</h4>
-              <p>${this.description}</p>
-            </div>
-          </div>
-          <div class="card-content">
-            <div class="card-property">
-              <p style="color: #969696;">Status</p>
-              <p>${this.status}</p>
-            </div>
-            <div class="card-property">
-              <p style="color: #969696;">Role</p>
-              <p>${this.userRole}</p>
-            </div>
-            <div class="card-property">
-              <p style="color: #969696;">Cost</p>
-              <p>$${this.cost}</p>
-            </div>
-            <div class="card-property">
-              <p style="color: #969696;">Estimated Progress</p>
-              <p>${this.progress * 100}%</p>
-            </div>
-          </div>
-    `;
-  }
+  
   setTaskUI () {
     const container = document.getElementById("to-do-list")
     //console.log(container)
