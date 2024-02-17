9b722f4f2b9a0c1efaf65b9d417acbebf54d8572
Curiosit <mailing.bot@gmail.com>
2024-02-17 23:36:07 +0100

    added firebase materials

---
+++ src/classes/Material.ts
@@ -0,0 +1,4 @@
+export interface IMaterial {
+    name: string;
+
+  }
--- src/react-components/Materials.tsx
+++ src/react-components/Materials.tsx
@@ -4,14 +4,16 @@ import { ProjectsManager } from "../classes/ProjectsManager"
 import { formatDate, setupModal, showModal } from "../utils/utils"
 import { renderProgress } from "../utils/utils"
 import { IFCViewer } from "./IFCViewer"
-import { deleteDocument } from "../firebase"
+import { deleteDocument, getCollection } from "../firebase"
 import { Modal } from "./Modal"
 import { downloadData } from "../utils/materialdata"
+import { IMaterial } from "../classes/Material"
+import * as Firestore from "firebase/firestore"
 
 
 
 
-  
+const materialsCollection = getCollection<IMaterial>("/materials")
 interface Props {
     
 }
@@ -21,7 +23,7 @@ interface Props {
 export function MaterialsPage(props: Props) {
     
     
-    const [materialData, setMaterialData] = React.useState([]);
+    const [materialData, setMaterialData] = React.useState([{}]);
     const [initialized, setInitialized] = React.useState(false);
     
     
@@ -31,17 +33,49 @@ export function MaterialsPage(props: Props) {
         
         num += 1
 
-        console.log(material.name)
-        return  <p>Material {num}: {material.name}</p>
+        console.log(material)
+        
+        let valueOfA1A2A3 = material.gwp?.A1A2A3;
+        if (valueOfA1A2A3 !== undefined) {
+            console.log("Value of A1A2A3:", valueOfA1A2A3);
+        } else {
+            console.error("Property 'A1A2A3' not found in the material data or 'gwp' is undefined.", material.name);
+        }
+        return  <p>{material.name} {valueOfA1A2A3} kgCO2e / {material.unit}</p>
         
     })
     
+    const getFirestoreMaterials = async () => {
+        
+        const firebaseMaterials = await Firestore.getDocs(materialsCollection)
+        const materialList = [{}]
+        for (const doc of firebaseMaterials.docs) {
+            const data = doc.data() 
+            const material: IMaterial = {
+                ...data,
+                
+            }
+            try {
+                
+                console.log(material)
+                console.log(material.name)
+                materialList.push(material)
+                //materialList.push(material)
+            }
+            catch (error) {
+                console.log(error)
+            }
+            
+        }
+        setMaterialData(materialList)
+        return firebaseMaterials.docs
+    }
     
     
     React.useEffect(() => {
         const getData = async () => {
-            const data = await downloadData()
-            setMaterialData(data.materials)
+            const data = await getFirestoreMaterials()
+            //setMaterialData(data)
             if(data) {
                 setInitialized(true)
             }
