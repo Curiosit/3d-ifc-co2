5fab5e92d68a4c5ee30c9f4dfa765011cbdfe522
Curiosit <mailing.bot@gmail.com>
2024-02-17 19:12:57 +0100

    update path

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
-import { formatDate, modifyDateInput } from "./utils/utils"
-import { showModal } from "./utils/utils"
+
 import * as Router from "react-router-dom"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { Sidebar } from "./react-components/Sidebar"
 import { ProjectsPage } from "./react-components/ProjectsPage"
 import { ProjectDetailsPage } from "./react-components/ProjectDetailsPage"
-
 import { ViewerProvider } from "./react-components/IFCViewer"
-import { FragmentsGroup } from "bim-fragment"
-
-import * as OBC from "openbim-components"
-import { TodoCreator } from "./bim-components/TodoCreator"
-import { SimpleQto } from "./bim-components/SimpleQto"
 import { ProjectsManager } from "./classes/ProjectsManager"
 import { E404 } from "./react-components/E404"
 import { Modal } from "./react-components/Modal"
